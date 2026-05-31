14215e54451dfc84676727debf8c48e94276128a
nahidxcode <nahidnm3@gmail.com>
2026-05-31 20:28:36 +0600

    Implement plaid link and dwolla

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ const Home = async () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn.name || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
           />
 
